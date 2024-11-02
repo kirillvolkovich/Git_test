afacf2dd6cfa5943e7cebc9dd835c8e3f063e474
kvolk04 <54364722+kvolk04@users.noreply.github.com>
2024-11-02 22:59:23 +0300

    second commit

--- test_git/src/Main.java
+++ test_git/src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int a = 0;
         int b = 25;
-        System.out.println(a);
+        int c = 52;
         System.out.println(b);
+        System.out.println(c);
     }
 }
